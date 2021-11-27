9d9ebd7ca436210fcc37c0bf0719462a32611d75
aleksa12432 <a.lazarevic.km@gmail.com>
2021-11-27 11:33:28 +0100

    Added some patches, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 Fork of igor37's dwm-rounded corners with more-default dwm keybindings. 
-
+Added patches on top:
+> urgentborder
+> selfrestart
+> Functionality with pywal
--- config.h
+++ config.h
@@ -13,16 +13,17 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "-misc-open sans-medium-r-normal-*-17-120-100-100-p-0-*-*" };
 static const char dmenufont[]       = "-misc-open sans-medium-r-normal-*-17-120-100-100-p-0-*-*";
-static const char col_gray1[]       = "#002249";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#609bcf";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-};
+#include "/home/jzma/.cache/wal/colors-wal-dwm.h"
+//static const char col_gray1[]       = "#002249";
+//static const char col_gray2[]       = "#444444";
+//static const char col_gray3[]       = "#bbbbbb";
+//static const char col_gray4[]       = "#eeeeee";
+//static const char col_cyan[]        = "#609bcf";
+//static const char *colors[][3]      = {
+//	/*               fg         bg         border   */
+//	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+//	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+//};
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
@@ -62,11 +63,13 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", norm_bg, "-nf", norm_fg, "-sb", norm_border, "-sf", sel_fg, NULL };
 static const char *termcmd[]  = { "terminator", NULL };
 static const char *screenshot[]     = { "flameshot", "gui", "-p", "/tmp", NULL };
 static const char *cirilica[] = { "setxkbmap", "rs" , NULL};
 static const char *latinica[] = { "setxkbmap", "us" , NULL};
+
+#include "selfrestart.c"
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -109,6 +112,7 @@ static Key keys[] = {
         { MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	{MODKEY,			XK_c,     spawn,	   {.v = cirilica } },
 	{MODKEY,			XK_Cyrillic_tse,     spawn,	   {.v = latinica} },
+	{MODKEY|ShiftMask,              XK_r,     self_restart, {0} },
 };
 
 /* button definitions */
@@ -127,4 +131,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
